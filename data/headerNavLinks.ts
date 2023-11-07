3dcf7c3d0228f42a17cf916b426137a4ba3c3213
Johan Vilela <johanvilela@hotmail.com>
2023-11-07 14:24:13 -0300

    refactor: update headerNavLinks

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,8 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/projects', title: 'Projetos' },
+  { href: '/about', title: 'Sobre' },
 ]
 
 export default headerNavLinks
